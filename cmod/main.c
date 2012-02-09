7d2307ef893c7a449ccf61b5b876048fba64d822
Josep Llodrà <jlg.hrtc@gmail.com>
2012-02-10 00:04:34 +0100

    Initial Commit

---
+++ cmod/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  cmod
+//
+//  Created by Josep Llodrà on 10/02/12.
+//  Copyright (c) 2012 Josep Llodrà Grimalt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
